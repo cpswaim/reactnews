6e1986343a8c930c98c57de8afa4071e1f9ee000
Cameron Swaim <cpswaim@gmail.com>
2015-02-21 12:43:08 -0500

    adding user link

--- src/app.js
+++ src/app.js
@@ -101,22 +101,6 @@ var NewsService = function() {
 // };
 };
 
-
-
-
-
-/**
- * This file provided by Facebook is for non-commercial testing and evaluation purposes only.
- * Facebook reserves all rights not expressly granted.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
- * FACEBOOK BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
- * ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
- * WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
- */
-
 var Article = React.createClass({
     render: function() {
         return (
@@ -126,7 +110,9 @@ var Article = React.createClass({
                         {this.props.number}. {this.props.title}
                     </h3>
                 </a>
-                <span>{this.props.score} points by {this.props.by}</span>
+                <span>{this.props.score} points by&nbsp;
+                    <a href={"https://news.ycombinator.com/user?id="+this.props.by}>{this.props.by}</a>
+                </span>
             </div>
         );
     }
